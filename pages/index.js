b39b506e95da15aeddc4707bb04637f16b52a7dc
Leonidas Esteban <leonidasesteban@gmail.com>
2019-04-28 15:57:20 -0700

    [add] sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,45 @@
 
-import MyCityMap from '../components/map'
+import { Component } from 'react'
+import styled from 'styled-components'
 
-function Home() {
-  return (
-    <section>
-      <MyCityMap />
-    </section>
-  )
+import MyCityMap from '../components/map'
+import Sidebar from '../components/sidebar'
+const Layout = styled.div`
+  display: grid;
+  border: 1px solid red;
+  grid-template-areas: 'header header'
+  'sidebar map';
+  grid-template-columns: 200px 1fr;
+  grid-template-rows: 100px 1fr;
+  height: 100vh;
+  .map-container {
+    position: relative;
+    grid-area: map;
+  }
+  .header-container {
+    grid-area: header;
+  }
+`
+
+class Home extends Component {
+  // static getInitialProps(req) {
+  //   return {
+  //     req.coo
+  //   }
+  // }
+  render() {
+    return (
+      <Layout>
+        <header className="header-container">
+          header
+        </header>
+        <Sidebar />
+        <div className="map-container">
+          <MyCityMap />
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default Home
